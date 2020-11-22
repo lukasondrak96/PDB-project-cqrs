a81a2d818cf92e8c1bc519e822c8b8fba34c8a74
Lukas Ondrak <ondry.cz@seznam.cz>
2020-11-22 16:23:44 +0100

    test commit

--- src/main/java/cz/vutbr/fit/pdb/projekt/sqlfeatures/group/GroupService.java
+++ src/main/java/cz/vutbr/fit/pdb/projekt/sqlfeatures/group/GroupService.java
@@ -3,5 +3,7 @@ package cz.vutbr.fit.pdb.projekt.sqlfeatures.group;
 import org.springframework.http.ResponseEntity;
 
 public interface GroupService {
+
     ResponseEntity<?> createNewGroup(GroupDto group);
+
 }
