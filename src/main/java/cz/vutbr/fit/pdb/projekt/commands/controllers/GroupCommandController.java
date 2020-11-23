96eee6118c4a6d86926e30d6e7e14f7c24740cb5
Tomáš Líbal <tomas.libal123@gmail.com>
2020-11-24 00:57:51 +0100

    Added endpoints to GroupCommandController

--- src/main/java/cz/vutbr/fit/pdb/projekt/commands/controllers/GroupCommandController.java
+++ src/main/java/cz/vutbr/fit/pdb/projekt/commands/controllers/GroupCommandController.java
@@ -1,12 +1,48 @@
 package cz.vutbr.fit.pdb.projekt.commands.controllers;
 
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
-@RequestMapping("/commands/group")
+@RequestMapping("/command/groups")
 public class GroupCommandController {
+    @PostMapping("new")
+    public ResponseEntity<?> createNewGroup() {
+        return null;
+    }
 
+    @PutMapping("{id}/edit")
+    public ResponseEntity<?> editGroup(@PathVariable(value = "id") int groupId) {
+        return null;
+    }
+
+    @DeleteMapping("{id}/delete")
+    public ResponseEntity<?> deleteGroup(@PathVariable(value = "id") int groupId) {
+        return null;
+    }
+
+    @PutMapping("{id}/change-state")
+    public ResponseEntity<?> changeGroupState(@PathVariable(value = "id") int groupId) {
+        return null;
+    }
+
+    @GetMapping("{id}/add-user/{userId}")
+    public ResponseEntity<?> addUserToGroup(@PathVariable(value = "id") int groupId,
+                                            @PathVariable(value = "userId") String userId) {
+        return null;
+    }
+
+    @GetMapping("{id}/remove-user/{userId}")
+    public ResponseEntity<?> removeUserToGroup(@PathVariable(value = "id") int groupId,
+                                               @PathVariable(value = "userId") String userId) {
+        return null;
+    }
+
+    @GetMapping("{id}/change-admin/{userId}")
+    public ResponseEntity<?> changeGroupAdmin(@PathVariable(value = "id") int groupId,
+                                              @PathVariable(value = "userId") String userId) {
+        return null;
+    }
 }
