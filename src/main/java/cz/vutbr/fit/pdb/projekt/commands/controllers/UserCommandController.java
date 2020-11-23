15d2735b6af89630131ee7b4ee387a1d2b3b6ffc
Tomáš Líbal <tomas.libal123@gmail.com>
2020-11-23 22:35:43 +0100

    Added endpoints to UserCommandController

--- src/main/java/cz/vutbr/fit/pdb/projekt/commands/controllers/UserCommandController.java
+++ src/main/java/cz/vutbr/fit/pdb/projekt/commands/controllers/UserCommandController.java
@@ -1,34 +1,43 @@
 package cz.vutbr.fit.pdb.projekt.commands.controllers;
 
 import cz.vutbr.fit.pdb.projekt.commands.services.UserCommandService;
-import cz.vutbr.fit.pdb.projekt.events.events.user.UserCreatedEvent;
-import cz.vutbr.fit.pdb.projekt.events.subscribers.UserEventSubscriber;
-import cz.vutbr.fit.pdb.projekt.features.nosqlfeatures.user.UserDocument;
-import cz.vutbr.fit.pdb.projekt.features.sqlfeatures.user.UserTable;
 import lombok.AllArgsConstructor;
-import org.greenrobot.eventbus.EventBus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
-@RequestMapping("/commands/user")
+@RequestMapping("/command/users")
 public class UserCommandController {
     private final UserCommandService userCommandService;
 
-    @GetMapping("test")
-    public void test() {
-        final EventBus eventBus = EventBus.getDefault();
+    @PostMapping("new")
+    public ResponseEntity<?> createNewUserAccount() {
+//        final EventBus eventBus = EventBus.getDefault();
+//
+//        final UserDocument userDocument = new UserDocument("testtesttest", "111", 111, null, null, null, null, null );
+//        new UserEventSubscriber<>(userDocument, eventBus);
+//
+//        final UserTable userTable = new UserTable("testtesttest", "111", 111, null, null);
+//        new UserEventSubscriber<>(userTable, eventBus);
+//
+//        final UserCreatedEvent createdEvent = new UserCreatedEvent(userCommandService);
+//        eventBus.post(createdEvent);
+        return null;
+    }
 
-        final UserDocument userDocument = new UserDocument("testtesttest", "111", 111, null, null, null, null, null );
-        new UserEventSubscriber<>(userDocument, eventBus);
+    @GetMapping("{id}/activate")
+    public ResponseEntity<?> activateUserAccount(@PathVariable(value = "id") int userId) {
+        return null;
+    }
 
-        final UserTable userTable = new UserTable("testtesttest", "111", 111, null, null);
-        new UserEventSubscriber<>(userTable, eventBus);
+    @GetMapping("{id}/deactivate")
+    public ResponseEntity<?> deactivateUserAccount(@PathVariable(value = "id") int userId) {
+        return null;
+    }
 
-        final UserCreatedEvent createdEvent = new UserCreatedEvent(userCommandService);
-        eventBus.post(createdEvent);
-
+    @PutMapping("{id}/edit")
+    public ResponseEntity<?> editUserAccount(@PathVariable(value = "id") int userId) {
+        return null;
     }
 }
